58a63b23470022b6c5de3cad86b0da11f0e9db13
AlexXi19 <alex2001314jjj@gmail.com>
2023-01-03 18:00:25 -0800

    comments on python

--- .local/scripts/sessions.py
+++ .local/scripts/sessions.py
@@ -1,14 +1,15 @@
 import subprocess
 
+# Don't judge the python thanks
+
 git_projects = subprocess.check_output("find ~/Desktop/CS -mindepth 1 -maxdepth 1 -type d", shell=True).splitlines()
 tmux_sessions = subprocess.check_output("tmux list-sessions -F \"#{session_name}\"", shell=True).splitlines()
 git_projects = [i.decode() for i in git_projects]
 tmux_sessions = [i.decode() for i in tmux_sessions]
 
-# Remove duplicate 
+# Remove duplicates
 session_list = git_projects
 git_projects_name_list = [dir.split("/")[-1] for dir in session_list]
-
 for session in tmux_sessions:
     if session not in git_projects_name_list: 
         session_list.append(session)
@@ -17,7 +18,5 @@ for session in tmux_sessions:
 session_list = filter(lambda x: len(x), session_list)
 
 # Write to output
-
-
 for i in session_list:
     print(i)
