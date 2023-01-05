9c28d052197a80725c27b0ec60624f3177597dac
AlexXi19 <alex2001314jjj@gmail.com>
2023-01-04 23:05:08 -0800

    more keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,7 +4,6 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sp', require('telescope.builtin').git_files, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>gd', require('telescope.builtin').lsp_references, { desc = '[G]et [R]eferences' })
 vim.keymap.set('n', '<leader>qw', require('telescope.builtin').oldfiles, { desc = '[G]et [R]eferences' })
 
 
--- .config/nvim/lua/alexxi/keymaps.lua
+++ .config/nvim/lua/alexxi/keymaps.lua
@@ -1,12 +1,12 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
+-- Moving visual blocks around 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- Navigating with pages 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -21,11 +21,15 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<Right>", "copilot#Accept(\"<CR>r\")", { expr = true, silent = true })
 
+-- Toggle search highlighting  
 vim.keymap.set("n", "<leader>/", ":set invhlsearch  \n")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer-full<CR>")
 
--- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Find and replace
+vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make Executable 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>fm", ":LspZeroFormat  \n")
@@ -33,3 +37,13 @@ vim.keymap.set("n", "<leader>fm", ":LspZeroFormat  \n")
 -- Remove bracket paragraph jumping from jump list  
 vim.api.nvim_set_keymap("n", "}", [[:keepjumps normal! }<cr>]], { noremap = true })
 vim.api.nvim_set_keymap("n", "{", [[:keepjumps normal! {<cr>]], { noremap = true })
+
+-- Toggle NvimTree file directory
+vim.keymap.set("n", "<leader>fd", ":NvimTreeToggle  \n")
+
+-- Git branch diff 
+vim.keymap.set("n", "<leader>gd", ":DiffviewOpen  \n")
+vim.keymap.set("n", "z", "zz")
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("C", "tabclose", {})
+
