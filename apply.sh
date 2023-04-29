6b32e7cebeb36593a2b64ff172b2161569e34b6f
AlexXi19 <alex2001314jjj@gmail.com>
2023-04-29 13:46:59 -0700

    nvim lsp enter select + neovim colors

--- apply.sh
+++ apply.sh
@@ -1,20 +1,20 @@
 # Applies the dot files in this repo, only applies shell scripts and neovim config
 
 # Bash scripts 
-mkdir -p ~/.local/scripts
-rsync -a .local/scripts/ ~/.local/scripts/
+# mkdir -p ~/.local/scripts
+# rsync -a .local/scripts/ ~/.local/scripts/
 
 # Themes and custom plugins
-mkdir -p .oh-my-zsh
-rsync -a .oh-my-zsh/custom/ ~/.oh-my-zsh/custom/ 
+# mkdir -p .oh-my-zsh
+# rsync -a .oh-my-zsh/custom/ ~/.oh-my-zsh/custom/ 
 
 # .zshrc
-cp .zshrc ~/.zshrc
-cp .profile ~/.profile
+# cp .zshrc ~/.zshrc
+# cp .profile ~/.profile
 
 # Tmux
 cp .tmux.conf ~/.tmux.conf
 
 # Neovim
-mkdir -p nvim
-rsync -a nvim/ ~/.config/nvim/ 
+mkdir -p ~/.config/nvim
+cp -r ./.config/nvim/ ~/.config/nvim/ 
