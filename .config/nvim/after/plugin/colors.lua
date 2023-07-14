05109b79b35d76c5f5c6b93e965772c102683005
alexxi19 <alex2001314jjj@gmail.com>
2023-07-14 15:59:42 -0700

    update nvim: persist colorscheme

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,12 +1,50 @@
-vim.cmd.colorscheme "github_dark_high_contrast"
+local default_colorscheme = "github_dark_high_contrast"
+
+-- Read color persisted 
+function readColor()
+  local file = io.open(COLORSCHEME_DIR, "r")
+  if file then
+    local firstLine = file:read("*line")
+    file:close()
+    return firstLine
+  else
+    return default_colorscheme
+  end
+end
+
+local colorscheme = readColor()
+
+vim.api.nvim_command("colorscheme " .. colorscheme)
 
 require("transparent").setup({
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
+  extra_groups = {
      "all"
   },
-  exclude_groups = {}, -- table: groups you don't want to clear
+  exclude_groups = {},
 })
 
 require("barbecue.ui").toggle(true)
 
+-- Persist colorscheme
+function handleColorChange()
+    local colorscheme = vim.api.nvim_exec('echo g:colors_name', true)
+
+    local file, err = io.open(COLORSCHEME_DIR, 'w')
+
+    if err then
+        vim.api.nvim_err_writeln('Failed to open file: ' .. COLORSCHEME_DIR .. '. Error: ' .. err)
+        return
+    end
+
+    file:write(colorscheme .. '\n')
+
+    file:close()
+end
+
+-- Autocommand to call persist method on colorscheme change
+vim.cmd([[
+    augroup ColorSchemeChange
+        autocmd!
+        autocmd ColorScheme * lua handleColorChange()
+    augroup END
+]])
--- .config/nvim/lua/alexxi/init.lua
+++ .config/nvim/lua/alexxi/init.lua
@@ -2,6 +2,9 @@ require("alexxi.packer")
 require("alexxi.sets")
 require("alexxi.keymaps")
 
+COLORSCHEME_DIR=os.getenv("HOME") .. "/.cache/nvim/colorscheme.txt"
+
+
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
