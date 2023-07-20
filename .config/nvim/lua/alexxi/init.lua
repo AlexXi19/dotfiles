f3c4b9130e96a44e9c2f7258fc9c4ef047429b2f
alexxi19 <alex2001314jjj@gmail.com>
2023-07-20 15:49:35 -0700

    fix(nvim): fix plugins dir

--- .config/nvim/lua/alexxi/init.lua
+++ .config/nvim/lua/alexxi/init.lua
@@ -7,4 +7,18 @@ require("alexxi.lazy")
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function load_configs()
+  local configs_path = vim.fn.stdpath('config') .. '/lua/alexxi/configs/*.lua'
+  local files = vim.fn.glob(configs_path, false, true)
+
+  for _, file in ipairs(files) do
+    local module = file:match(".+/([^/]+)%.lua$")
+    if module then
+      require('alexxi.configs.' .. module)
+    end
+  end
+end
+
+load_configs()
+
 vim.loader.enable()
