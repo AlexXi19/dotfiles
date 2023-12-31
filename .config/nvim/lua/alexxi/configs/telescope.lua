05942794654b1d0376644ef475ec321fd631f36f
AlexXi19 <alex2001314jjj@gmail.com>
2023-12-30 16:33:44 -0800

    chore: update find file

--- .config/nvim/lua/alexxi/configs/telescope.lua
+++ .config/nvim/lua/alexxi/configs/telescope.lua
@@ -5,28 +5,17 @@ local telescope_config = require("telescope.config")
 
 -- Do not preview files larger than 100kb
 local new_maker = function(filepath, bufnr, opts)
-  opts = opts or {}
+    opts = opts or {}
 
-  filepath = vim.fn.expand(filepath)
-  vim.loop.fs_stat(filepath, function(_, stat)
-    if not stat then return end
-    if stat.size > 100000 then
-      return
-    else
-      previewers.buffer_previewer_maker(filepath, bufnr, opts)
-    end
-  end)
-end
-
--- Find files: Falling back to find_files if git_files can't find a .git directory
-local find_files = function()
-  local opts = {}
-  vim.fn.system('git rev-parse --is-inside-work-tree')
-  if vim.v.shell_error == 0 then
-    require"telescope.builtin".git_files(opts)
-  else
-    require"telescope.builtin".find_files(opts)
-  end
+    filepath = vim.fn.expand(filepath)
+    vim.loop.fs_stat(filepath, function(_, stat)
+        if not stat then return end
+        if stat.size > 100000 then
+            return
+        else
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+        end
+    end)
 end
 
 -- Clone the default Telescope configuration
@@ -53,7 +42,7 @@ require('telescope').setup {
                 cwd_only = true,
             },
             find_files = {
-                find_command = { "rg", "--files", "--hidden", "--glob", "--unrestricted", "!**/.git/*"},
+                find_command = { "rg", "--files", "--hidden", "--glob", "--unrestricted", "!**/.git/*" },
                 hidden = true
             },
         },
@@ -66,10 +55,11 @@ require('telescope').setup {
 }
 
 -- Keymaps
-vim.keymap.set('n', '<C-p>', find_files)
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[P]ick [F]ile' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<C-G>', function() require('telescope.builtin').live_grep({ additional_args = { "-j1" } }) end, { desc = '[G]rep' })
+vim.keymap.set('n', '<C-G>', function() require('telescope.builtin').live_grep({ additional_args = { "-j1" } }) end,
+    { desc = '[G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sp', require('telescope.builtin').git_files)
 vim.keymap.set('n', '<leader>qw', require('telescope.builtin').oldfiles)
@@ -79,5 +69,5 @@ vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = 
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').git_branches, {})
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ts', require('telescope.builtin').builtin, {})
-vim.keymap.set('n', '<leader>sl', function () require('telescope').extensions.bookmarks.bookmarks({ selected_browser = "chrome" }) end, {})
-
+vim.keymap.set('n', '<leader>sl',
+    function() require('telescope').extensions.bookmarks.bookmarks({ selected_browser = "chrome" }) end, {})
